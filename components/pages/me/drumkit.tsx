e5c02c087641e563456c095e52ace216a0c40921
pikooli <>
2022-02-21 17:47:11 +0100

    finish drumkit

---
+++ components/pages/me/drumkit.tsx
@@ -0,0 +1,91 @@
+import React, { useEffect } from "react";
+import tom1 from "public/drumkit/sounds/tom-1.mp3";
+import tom2 from "public/drumkit/sounds/tom-2.mp3";
+import tom3 from "public/drumkit/sounds/tom-3.mp3";
+import tom4 from "public/drumkit/sounds/tom-4.mp3";
+import snare from "public/drumkit/sounds/snare.mp3";
+import kickBass from "public/drumkit/sounds/kick-bass.mp3";
+import crash from "public/drumkit/sounds/crash.mp3";
+import Title2 from "components/text/Title3";
+
+type Props = {};
+const Drumkit: React.FC<Props> = () => {
+  useEffect(() => {
+    window.document.addEventListener("keydown", (e) => {
+      const element: HTMLElement = document.getElementsByClassName(
+        e.key
+      )[0] as HTMLElement;
+      if (element) element.click();
+    });
+  }, []);
+
+  function playSound(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    if (e) {
+      const target = e.currentTarget;
+      target.style.borderColor = "#da0463";
+      let audio;
+      switch (target.classList[0]) {
+        case "w":
+          audio = new Audio(tom1); //change the source
+          break;
+        case "a":
+          audio = new Audio(tom2); //change the source
+          break;
+        case "s":
+          audio = new Audio(tom3); //change the source
+          break;
+        case "d":
+          audio = new Audio(tom4); //change the source
+          break;
+        case "j":
+          audio = new Audio(snare); //change the source
+          break;
+        case "k":
+          audio = new Audio(kickBass); //change the source
+          break;
+        case "l":
+          audio = new Audio(crash); //change the source
+          break;
+      }
+      if (audio) {
+        audio.play();
+        audio = null;
+      }
+      setTimeout(() => {
+        target.style.borderColor = "#404b69";
+      }, 500);
+    }
+  }
+
+  return (
+    <div className="drumkit">
+      <div className="set py-2">
+        <Title2 title="Here a little drum kit to pass time. 🥁" />
+        <p>You can click on it or use the keyboard. 👍</p>
+        <button className="w drum" onClick={(e) => playSound(e)}>
+          w
+        </button>
+        <button className="a drum" onClick={(e) => playSound(e)}>
+          a
+        </button>
+        <button className="s drum" onClick={(e) => playSound(e)}>
+          s
+        </button>
+        <button className="d drum" onClick={(e) => playSound(e)}>
+          d
+        </button>
+        <button className="j drum" onClick={(e) => playSound(e)}>
+          j
+        </button>
+        <button className="k drum" onClick={(e) => playSound(e)}>
+          k
+        </button>
+        <button className="l drum" onClick={(e) => playSound(e)}>
+          l
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Drumkit;
--- components/pages/me/index.tsx
+++ components/pages/me/index.tsx
@@ -1,22 +1,25 @@
 import React from "react";
-import Image from "next/image";
+import Drumkit from "components/pages/me/drumkit";
 
 type Props = {};
 
 const Me: React.FC<Props> = () => {
   return (
-    <div className="d-flex">
-      <Image
-        src="/pascal_zhang.jpg"
-        width={300}
-        height={300}
-        alt="pascal zhang"
-        className="rounded me-3"
-      />
-      <p>
-        Who i am ? a vampire ? a chinese guy ? a developper ? all this ?
-        Maybeeeeee ???
-      </p>
+    <div>
+      <div className="d-flex">
+        <img
+          src="/pascal_zhang.jpg"
+          className="rounded me-3"
+          width={300}
+          height={300}
+          alt="pascal zhang"
+        />
+        <p>
+          Who i am ? a vampire ? a chinese guy ? a developper ? all this ?
+          Maybeeeeee ???
+        </p>
+      </div>
+      <Drumkit />
     </div>
   );
 };
