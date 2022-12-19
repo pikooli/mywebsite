da2dcd375b0d7f6766e075189c1d1eb11503d3c7
pikooli <>
2022-12-19 23:31:43 +0100

    fix deployement

--- pages/wonder_game.tsx
+++ pages/wonder_game.tsx
@@ -1,7 +1,10 @@
 import dynamic from "next/dynamic";
 import Layout from "components/Layout";
+import { WonderGame } from "components/pages/pixiJs/WonderGame";
 
-const WonderGameComponentWithNoSSR = dynamic(
+const WonderGameComponentWithNoSSR = dynamic<
+  React.ComponentProps<typeof WonderGame>
+>(
   () =>
     import("components/pages/pixiJs/WonderGame/index").then(
       (mod) => mod.WonderGame
