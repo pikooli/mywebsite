3f57630ede5d51e441e8e17d04f2c984d1b99725
pikooli <>
2023-01-20 19:13:07 +0100

    fix tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       transformOrigin: {
-        0: "0%",
+        0: '0%',
       },
     },
   },
-  plugins: [require("@tailwindcss/forms")],
+  plugins: [require('@tailwindcss/forms')],
 };
