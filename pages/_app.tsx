c20e785d8d07c12c918eba4b192e4191262d038f
pascal Zhang <panamepoul@gmail.com>
2025-01-06 14:45:17 +0100

    fixing google tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 "use client";
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 
 import '../styles/globals.scss';
 import { AppProvider } from 'src/context/AppContext';
@@ -11,9 +12,26 @@ config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+    <>
+    <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-J6DL506TK0'"
+      ></Script>
+      <Script
+        dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+          gtag('config', 'G-J6DL506TK0');
+        `,
+        }}
+      />
     <AppProvider>
       <Component {...pageProps} />
     </AppProvider>
+    </>
   );
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import Script from 'next/script';
 
 export default function Header({ title }: { title?: string }) {
   return (
@@ -7,21 +6,6 @@ export default function Header({ title }: { title?: string }) {
       <title>{title}</title>
       <meta name="description" content="Website of Pascal Zhang" />
       <link rel="icon" href="/favicon.ico" />
-      <Script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-J6DL506TK0'"
-      ></Script>
-      <Script
-        dangerouslySetInnerHTML={{
-          __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-          gtag('config', 'G-J6DL506TK0'');
-        `,
-        }}
-      />
     </Head>
   );
 }
