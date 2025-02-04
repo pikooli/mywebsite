765e7de368e9e0e4bc245e6b1fb5a5dc3e41d23f
pascal Zhang <panamepoul@gmail.com>
2025-02-04 12:26:26 +0100

    react monitor perf

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      {/* <head>
+        <script src="https://unpkg.com/react-scan/dist/auto.global.js" async />
+      </head> */}
       <body
         className={`${merriweather.variable} ${openSans.variable} antialiased`}
       >
